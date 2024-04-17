95ee6dfc20544898964ee339fbb14607a7687b10
nakanuma <k023g0121@g.neec.ac.jp>
2024-04-17 16:34:46 +0900

    02_01_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,9 @@
 #include "ViewProjection.h"
 #include "WorldTransform.h"
 
+// MyClass
+#include "Player.h"
+
 /// <summary>
 /// ゲームシーン
 /// </summary>
@@ -47,4 +50,16 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	// 3Dモデルデータ
+	Model* model_ = nullptr;
+
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// 自キャラ
+	Player* player_ = nullptr;
 };
