39192c877904302da98d293434af2ccbf4839002
nakanuma <k023g0121@g.neec.ac.jp>
2024-04-18 21:59:06 +0900

    MyMathとWorldTransformを更新

--- MyMath.h
+++ MyMath.h
@@ -10,23 +10,23 @@ public:
 	///	4x4行列
 	///
 
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4 m2);
+	static Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4 m2);
 
 	// 単位行列を書き込む
-	Matrix4x4 Identity();
+	static Matrix4x4 Identity();
 
-	Matrix4x4 Scaling(Vector3 scale);
+	static Matrix4x4 Scaling(Vector3 scale);
 
-	Matrix4x4 Translation(Vector3 translation);
+	static Matrix4x4 Translation(Vector3 translation);
 
-	Matrix4x4 RotationX(float rad);
+	static Matrix4x4 RotationX(float rad);
 
-	Matrix4x4 RotationY(float rad);
+	static Matrix4x4 RotationY(float rad);
 
-	Matrix4x4 RotationZ(float rad);
+	static Matrix4x4 RotationZ(float rad);
 
-	Matrix4x4 RotationXYZ(float rotateX, float rotateY, float rotateZ);
+	static Matrix4x4 RotationXYZ(float rotateX, float rotateY, float rotateZ);
 
 	// アフィン変換行列の作成
-	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+	static Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 };
